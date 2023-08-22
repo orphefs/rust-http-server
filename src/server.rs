2ee3f1d85a4f7f49ffd6c82b27ed3717143729a3
Orfeas Kypris <kypriso@gmail.com>
2023-08-22 23:02:37 +0300

    log incoming requests

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,11 @@
 use std::net::TcpListener;
+use std::io::Read;
 
 pub struct Server {
     addr: String,
 }
+
+fn arr(a: &[u8]) {}
 
 //implementation block (methods go here)
 impl Server {
@@ -18,12 +21,24 @@ impl Server {
         // `self` will be deallocated when the run() function exits.
         println!("Listening on {}", self.addr);
 
-        let listener = TcpListener::bind(&self.addr).unwrap();
+        let listener = TcpListener::bind(&self.addr).unwrap(); // unwrap terminates the program if the Result is an error
 
         'outer: loop {
             // here we are labeling the loop
             // "loop" is same as "while true" (infinite loop)
-            break;
+
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(buffer));
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
         }
     }
 }
